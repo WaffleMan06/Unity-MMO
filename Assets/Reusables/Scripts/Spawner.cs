76becc9680d2c160a684f951877fdf9d54a3bc1b
WaffleMan06 <87388332+WaffleMan06@users.noreply.github.com>
2024-07-21 16:48:53 -0400

    Basic Spawner + rearranged files

---
+++ Assets/Reusables/Scripts/Spawner.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject spawnedCreature = null;
+
+    [Header("Spawn conditions")]
+    public bool spawnConditionsMet = false;
+
+
+    void Start()
+    {
+        
+    }
+}
